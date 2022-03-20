f0cc9fe133499c0b06c8c54baf41f5f937e92ba5
tonge <tonge@BERKAI>
2022-03-20 13:57:31 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp8/Karakter.cs
@@ -0,0 +1,35 @@
+﻿using SFMLResim;
+
+namespace Oyuncu
+{
+
+	public static class Karakter
+	{
+		public static void CharKontrol(ref float a, ref float b)
+		{
+			if (Resim.KlavyeBasili(Resim.Klavye.W))
+				a -= 200 * Resim.FrameTime;
+			else if (Resim.KlavyeBasili(Resim.Klavye.S))
+				a += 200 * Resim.FrameTime;
+			else if (Resim.KlavyeBasili(Resim.Klavye.A))
+				b -= 200 * Resim.FrameTime;
+			else if (Resim.KlavyeBasili(Resim.Klavye.D))
+				b += 200 * Resim.FrameTime;
+		}
+		public static int Oyuncu()
+		{
+			return Resim.Ekle("res\\player.png");
+
+		}
+		/*public static void Ates(int res,ref float x, ref float y)
+		{ 
+			if(Resim.KlavyeBasili(Resim.Klavye.Space))
+			{
+				Resim.Ciz(res, x, y+76, 50, 50);
+
+				y += 10 * Resim.FrameTime;
+				
+			}
+		}*/
+	}
+}
---
+++ WindowsFormsApp8/Program.cs
@@ -0,0 +1,33 @@
+﻿using Oyuncu;
+using SFMLResim;
+namespace mainclass
+{
+
+	public static class Program
+	{
+		public static void Main()
+		{
+
+			float x = 400, y = 400, r = 25;
+			Resim.Yeni(800, 600, "oyun");
+			Resim.SetFPSLimit(60);
+
+
+			int bgResim = Resim.Ekle("res\\bg.png");
+			int laser = Resim.Ekle("res\\laser_beam.png");
+			while (Resim.Acik())
+			{
+				Resim.Baslat();
+
+				Resim.Ciz(bgResim, 0, 0, 800, 600);
+				Resim.Ciz(Karakter.Oyuncu(), x - r, y - r, 2 * r, 2 * r);
+				Karakter.CharKontrol(ref y, ref x);
+				//Karakter.Ates(laser,ref x,ref y);
+				Resim.YaziYaz("FPS:" + (int)(1.0 / Resim.FrameTime), 0, 0, 50);
+				Resim.Bitir();
+
+			}
+
+		}
+	}
+}
